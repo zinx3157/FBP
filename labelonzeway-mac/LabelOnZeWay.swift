193632f7d594756da79f6346c1b03d52b4b4d736
zinx3157 <125181972+zinx3157@users.noreply.github.com>
2026-09-09 09:28:09 +0300

    Add native Mac V7 web app wrapper

---
+++ labelonzeway-mac/LabelOnZeWay.swift
@@ -0,0 +1,42 @@
+import Cocoa
+import WebKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate, WKNavigationDelegate {
+    var window: NSWindow!
+    var webView: WKWebView!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let config = WKWebViewConfiguration()
+        config.websiteDataStore = .default()
+        webView = WKWebView(frame: .zero, configuration: config)
+        webView.navigationDelegate = self
+        webView.setValue(false, forKey: "drawsBackground")
+
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 1440, height: 900),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.title = "LabelOnZeWay V7"
+        window.minSize = NSSize(width: 1024, height: 700)
+        window.center()
+        window.contentView = webView
+        window.makeKeyAndOrderFront(nil)
+
+        if let url = URL(string: "https://zinx3157.github.io/FBP/labelonzeway-v7/?v=mac-v7-runtime-20260909") {
+            webView.load(URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30))
+        }
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+app.setActivationPolicy(.regular)
+app.activate(ignoringOtherApps: true)
+app.run()
