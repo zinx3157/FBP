56f5d818114c3a8c32bc6b21cbd9a4be8def3a83
zinx3157 <125181972+zinx3157@users.noreply.github.com>
2026-09-12 19:09:28 +0300

    Point Mac wrapper to final V7 runtime

--- labelonzeway-mac/LabelOnZeWay.swift
+++ labelonzeway-mac/LabelOnZeWay.swift
@@ -24,7 +24,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, WKNavigationDelegate {
         window.contentView = webView
         window.makeKeyAndOrderFront(nil)
 
-        if let url = URL(string: "https://zinx3157.github.io/FBP/labelonzeway-v7/?v=mac-v7-runtime-20260909") {
+        if let url = URL(string: "https://zinx3157.github.io/FBP/labelonzeway-v7/?v=final-20260912-17&app=mac-final") {
             webView.load(URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30))
         }
     }
